9fbba88ce3ff60fa9fc8a1420f75cd5640f97f37
dmike <cipmiky@gmail.com>
2021-04-04 19:44:02 +0200

    feat: add multiboot section + vga print ok

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,5 +1,6 @@
+#![feature(asm)]
 #![no_std] // don't link the Rust standard library
 
 pub fn it_works() {
-    assert_eq!(2 + 2, 4);
+    unsafe { asm!("mov dword ptr [0xb8000], 0x2f4b2f4f"); }
 }
