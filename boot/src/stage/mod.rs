59f0b7432d0aab83660b55503a26aeea1eda0d80
dmike <cipmiky@gmail.com>
2021-05-16 01:14:13 +0200

    feat(boot): entering into long mode.

--- boot/src/stage/mod.rs
+++ boot/src/stage/mod.rs
@@ -1,5 +1,7 @@
-global_asm!(include_str!("stage1.S"));
-
+#[cfg(target_arch = "x86_64")]
+global_asm!(include_str!("x86_64/stage1.S"));
 #[cfg(target_arch = "x86_64")]
 global_asm!(include_str!("x86_64/check_system.S"));
+#[cfg(target_arch = "x86_64")]
+global_asm!(include_str!("x86_64/stage2.S"));
 
